5be8bacdc27aabb8347f39021d720046ebf68d6f
alojsb <alojsbirmingham@gmail.com>
2022-02-03 23:33:01 +0100

    3-conditional-rendering done

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,33 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  // short-circuit evaluation
+  // choose one of the next 2 lines (the other has to be commented out)
+  const [text, setText] = useState('');
+  // const [text, setText] = useState('zum');
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  // short-circuit example 2
+  const [isError, setIsError] = useState(false);
+
+  return (
+    <div>
+      <h2>short circuit example</h2>
+      <p>firstValue: {firstValue}</p>
+      <p>secondValue: {secondValue}</p>
+      <hr />
+      <h2>short circuit II example</h2>
+      <p>Is there an error?</p>
+      <div>{isError || <h4>there is no error</h4>}</div>
+      <hr />
+      <h2>ternary operator example</h2>
+      <div>{isError ? <h5>error error omg</h5> : <h5>no error here</h5>}</div>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+    </div>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <button className='btn' onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </div>
+  );
 };
 
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    // the use of this cleanup function is crucial
+    return () => window.removeEventListener('resize', checkSize);
+  }, []);
+
+  const style = {
+    color: 'limegreen',
+    width: '70%',
+    backgroundColor: 'darkgreen',
+    margin: '10vw auto',
+    padding: '4vw',
+    fontSize: '8vw',
+  };
+  return (
+    <section style={style}>
+      <h6>window</h6>
+      <h6>size: {size}</h6>
+    </section>
+  );
+};
 export default ShowHide;
